3a4823dee45e9425c0d2978a62560c08693a6c05
Venus Guerrero <guerrerovenusmarie_bsit@plmun.edu.ph>
2025-02-22 15:41:51 +0800

    Submitting Python Week 05 - Laboratory # 19

---
+++ grade_calculator_functions.py
@@ -0,0 +1,27 @@
+def get_student_score():
+    while True:
+        try:
+            score = float(input("Enter your score: "))
+            if 0 <= score <= 100:
+                return score
+            else:
+                print("Please enter a valid score between 0 and 100.")
+        except ValueError:
+            print("Invalid input. Please enter a numeric value.")
+
+def calculate_grade(score):
+    if score >= 90:
+        return 'A'
+    elif score >= 80:
+        return 'B'
+    elif score >= 70:
+        return 'C'
+    elif score >= 60:
+        return 'D'
+    else:
+        return 'F'
+
+score = get_student_score()
+grade = calculate_grade(score)
+print(f"Your Grade is: {grade}")
+    
