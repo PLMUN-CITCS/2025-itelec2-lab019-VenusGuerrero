c9f2e2b5dc70c186705709b2d7e7c0bdde1c434b
Venus Guerrero <guerrerovenusmarie_bsit@plmun.edu.ph>
2025-02-22 16:40:48 +0800

    Submitting Python Week 05 - Laboratory # 19

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -21,6 +21,10 @@ def calculate_grade(score):
     else:
         return 'F'
 
-score = get_student_score()
-grade = calculate_grade(score)
+
+score: float = get_student_score()
+grade: str = calculate_grade(score)
 print(f"Your Grade is: {grade}")
+
+
+
